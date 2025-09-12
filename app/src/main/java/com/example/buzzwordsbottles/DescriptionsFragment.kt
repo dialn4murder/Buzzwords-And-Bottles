97c57955b67c277f48b681e514d243926c7aad93
niah-stack <dylanchampion596@hotmail.com>
2025-09-12 22:52:39 +0100

    Initial recycler view setup

--- app/src/main/java/com/example/buzzwordsbottles/DescriptionsFragment.kt
+++ app/src/main/java/com/example/buzzwordsbottles/DescriptionsFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.buzzwordsbottles.adapters.DescriptionAdapter
 import com.example.buzzwordsbottles.databinding.FragmentCameraBinding
 
 
@@ -13,6 +16,8 @@ class DescriptionsFragment : Fragment() {
      * Initialises binding for login fragment
      */
     private lateinit var binding: FragmentCameraBinding
+    private lateinit var layoutManager: RecyclerView.LayoutManager
+    private lateinit var adapter: DescriptionAdapter
 
 
     override fun onCreateView(
@@ -22,5 +27,18 @@ class DescriptionsFragment : Fragment() {
         binding = FragmentCameraBinding.inflate(layoutInflater)
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        layoutManager = LinearLayoutManager(context)
+
+//        binding.
+//
+//        // Initialises the adapter and recycler view
+//        binding.gamesRecyclerView.layoutManager = layoutManager
+//        adapter = RecyclerAdapter(this)
+//        binding.gamesRecyclerView.adapter = adapter
     }
 }
---
+++ app/src/main/java/com/example/buzzwordsbottles/adapters/DescriptionAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.buzzwordsbottles.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.buzzwordsbottles.R
+import com.example.buzzwordsbottles.classes.Descriptions
+import com.example.buzzwordsbottles.databinding.DescriptionLayoutBinding
+
+class DescriptionAdapter : RecyclerView.Adapter<DescriptionAdapter.ViewHolder>() {
+    var list = mutableListOf<Descriptions>()
+    init {
+
+
+    }
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        val binding = DescriptionLayoutBinding.bind(itemView)
+        init {
+//            itemView.setOnClickListener{
+//
+//                listener.onClick(list[bindingAdapterPosition])
+//            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.description_layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val cardView = holder.binding
+        val item = list[position]
+
+//        val normalPrice = item.normalPrice.toString()
+//        val salePrice = item.salePrice.toString()
+//
+//        cardView.title.text = item.title
+//        cardView.originalPrice.text = "£$normalPrice"
+//        cardView.salePrice.text = "£$salePrice"
+    }
+
+    fun addItem(descriptions: Descriptions){
+        list.add(descriptions)
+        notifyItemInserted(list.lastIndex)
+    }
+
+}
+
