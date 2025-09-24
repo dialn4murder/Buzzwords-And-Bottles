49753cc858006eef84e254ed863c2da291c4fcdd
niah-stack <dylanchampion596@hotmail.com>
2025-09-24 13:46:46 +0100

    Fixing gradle issues

--- app/src/main/java/com/example/buzzwordsbottles/DescriptionScreen.kt
+++ app/src/main/java/com/example/buzzwordsbottles/DescriptionScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.NavController
 import com.example.buzzwordsbottles.classes.SharedViewModel
 
