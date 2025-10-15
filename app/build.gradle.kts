15883071c0b0459acd589d1d26f7518204883458
niah-stack <dylanchampion596@hotmail.com>
2025-10-15 22:44:37 +0100

    Initial new ML

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,9 @@ android {
 }
 
 dependencies {
+    implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.4")
+    implementation("org.tensorflow:tensorflow-lite-gpu-delegate-plugin:0.4.4")
+    implementation("org.tensorflow:tensorflow-lite-gpu:2.17.0")
     implementation(libs.androidx.camera.core)
     implementation(libs.androidx.camera.compose)
     implementation(libs.androidx.camera.lifecycle)
